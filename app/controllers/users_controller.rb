ed10ab8c8652d0849fbbec1c4eb9912ca3ddaa17
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-11-10 08:59:47 +0200

    added form_tag form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
-    def new
-    end
-    def create
-      @user = User.new(user_params)
+  def new 
+    
+  end
 
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new
-      end
-    end   
+  def create
+    @user = User.new(user_params)
 
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
